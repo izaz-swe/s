fb5a7cb1b3a4c6a4692098c10be3d9376251ce37
izaz-swe <izaz532963@gmail.com>
2024-04-26 10:31:17 +0600

    api changed

--- src/configs/variable.js
+++ src/configs/variable.js
@@ -1,2 +1,2 @@
-// export const api = "https://soil-to-factory.onrender.com/v1";
-export const api = "http://localhost:8088/v1";
+export const api = "https://soil-to-factory.onrender.com/v1";
+// export const api = "http://localhost:8088/v1";
